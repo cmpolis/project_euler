5502b92be993da1baf030f94deeada7bbc14b58b
Chris Polis <cmpolis@gmail.com>
2010-08-14 12:55:16 -0700

    Updated algorithm for problem7, runtime went from ~40sec to ~1sec

--- Problem7.rb
+++ Problem7.rb
@@ -1,9 +1,11 @@
 class Fixnum
   def prime?
-    (2..self/2).all? { |i| self % i != 0 }
+    # Only test up to square root, alot faster than up to self/2
+    (3..Math.sqrt(self)).all? { |i| self % i != 0 }
   end
 end
 
+startTime = Time.now
 prime_count = 1
 test_num = 3 #Assume 2 is prime, allows checking odds only 
 while prime_count < 10001 do
@@ -11,3 +13,4 @@ while prime_count < 10001 do
   test_num += 2 #Only test odds
 end
 puts test_num - 2 #Goes one iteration past 10001st prime
+puts Time.now - startTime
